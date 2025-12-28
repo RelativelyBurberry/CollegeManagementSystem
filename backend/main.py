54bb10f7d6d35cf7001e73fb364be1dd3fbefb9a
RelativelyBurberry <saxenaanipun@gmail.com>
2025-12-29 00:07:59 +0530

    cors moment

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "http://127.0.0.1:8000/",
+        "https://collegemanagementsystem-frontend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
